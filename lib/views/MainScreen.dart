e8c2d802072ffdb6fd16de62fdbc64f90c9e68bf
Gimosyan Azat <azat_g@mail.ru>
2022-02-01 21:21:43 +0400

    fix

--- lib/views/MainScreen.dart
+++ lib/views/MainScreen.dart
@@ -38,17 +38,17 @@ class _MainScreenState extends State<MainScreen> {
   }
 
   Future<int> returnOneAsync() async {
-    await Future<int>.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 3));
     return 1;
   }
 
   Future<int> returnTwoAsync() async {
-    await Future<int>.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 3));
     return 2;
   }
 
   Future<int> returnThreeAsync() async {
-    await Future<int>.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 3));
     return 3;
   }
 }
